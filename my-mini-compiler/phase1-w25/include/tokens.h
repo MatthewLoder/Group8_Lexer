d24d510401a06e734db09650e13978a46dbd0d76
Matthew Loder <matthewloder@Matthews-MacBook-Pro.local>
2025-02-04 21:16:20 -0500

    fixxed indents

--- my-mini-compiler/phase1-w25/include/tokens.h
+++ my-mini-compiler/phase1-w25/include/tokens.h
@@ -15,32 +15,34 @@
  * - Delimiters
  */
 typedef enum {
-    TOKEN_EOF,
-    TOKEN_NUMBER,     // e.g., "123", "456"
-    TOKEN_OPERATOR,   // e.g., "+", "-"
-    TOKEN_ERROR
+  TOKEN_EOF,
+  TOKEN_NUMBER,   // e.g., "123", "456"
+  TOKEN_OPERATOR, // e.g., "+", "-"
+  TOKEN_ERROR
 } TokenType;
 
 /* Error types for lexical analysis
- * TODO: Add more error types as needed for your language - as much as you like !!
+ * TODO: Add more error types as needed for your language - as much as you like
+ * !!
  */
 typedef enum {
-    ERROR_NONE,
-    ERROR_INVALID_CHAR,
-    ERROR_INVALID_NUMBER,
-    ERROR_CONSECUTIVE_OPERATORS
+  ERROR_NONE,
+  ERROR_INVALID_CHAR,
+  ERROR_INVALID_NUMBER,
+  ERROR_CONSECUTIVE_OPERATORS
 } ErrorType;
 
 /* Token structure to store token information
  * TODO: Add more fields if needed for your implementation
- * Hint: You might want to consider adding line and column tracking if you want to debug your lexer properly.
- * Don't forget to update the token fields in lexer.c as well
+ * Hint: You might want to consider adding line and column tracking if you want
+ * to debug your lexer properly. Don't forget to update the token fields in
+ * lexer.c as well
  */
 typedef struct {
-    TokenType type;
-    char lexeme[100];   // Actual text of the token
-    int line;           // Line number in source file
-    ErrorType error;    // Error type if any
+  TokenType type;
+  char lexeme[100]; // Actual text of the token
+  int line;         // Line number in source file
+  ErrorType error;  // Error type if any
 } Token;
 
 #endif /* TOKENS_H */
--- my-mini-compiler/phase1-w25/src/lexer/lexer.c
+++ my-mini-compiler/phase1-w25/src/lexer/lexer.c
@@ -1,9 +1,9 @@
 /* lexer.c */
+#include "../../include/tokens.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-#include "../../include/tokens.h"
 
 // Line tracking
 static int current_line = 1;
@@ -11,20 +11,20 @@ static char last_token_type = 'x'; // For checking consecutive operators
 
 /* Print error messages for lexical errors */
 void print_error(ErrorType error, int line, const char *lexeme) {
-    printf("Lexical Error at line %d: ", line);
-    switch (error) {
-        case ERROR_INVALID_CHAR:
-            printf("Invalid character '%s'\n", lexeme);
-            break;
-        case ERROR_INVALID_NUMBER:
-            printf("Invalid number format\n");
-            break;
-        case ERROR_CONSECUTIVE_OPERATORS:
-            printf("Consecutive operators not allowed\n");
-            break;
-        default:
-            printf("Unknown error\n");
-    }
+  printf("Lexical Error at line %d: ", line);
+  switch (error) {
+  case ERROR_INVALID_CHAR:
+    printf("Invalid character '%s'\n", lexeme);
+    break;
+  case ERROR_INVALID_NUMBER:
+    printf("Invalid number format\n");
+    break;
+  case ERROR_CONSECUTIVE_OPERATORS:
+    printf("Consecutive operators not allowed\n");
+    break;
+  default:
+    printf("Unknown error\n");
+  }
 }
 
 /* Print token information
@@ -33,112 +33,114 @@ void print_error(ErrorType error, int line, const char *lexeme) {
  */
 
 void print_token(Token token) {
-    if (token.error != ERROR_NONE) {
-        print_error(token.error, token.line, token.lexeme);
-        return;
-    }
+  if (token.error != ERROR_NONE) {
+    print_error(token.error, token.line, token.lexeme);
+    return;
+  }
 
-    printf("Token: ");
-    switch (token.type) {
-        case TOKEN_NUMBER:
-            printf("NUMBER");
-            break;
-        case TOKEN_OPERATOR:
-            printf("OPERATOR");
-            break;
-        case TOKEN_EOF:
-            printf("EOF");
-            break;
-        default:
-            printf("UNKNOWN");
-    }
-    printf(" | Lexeme: '%s' | Line: %d\n",
-           token.lexeme, token.line);
+  printf("Token: ");
+  switch (token.type) {
+  case TOKEN_NUMBER:
+    printf("NUMBER");
+    break;
+  case TOKEN_OPERATOR:
+    printf("OPERATOR");
+    break;
+  case TOKEN_EOF:
+    printf("EOF");
+    break;
+  default:
+    printf("UNKNOWN");
+  }
+  printf(" | Lexeme: '%s' | Line: %d\n", token.lexeme, token.line);
 }
 
 /* Get next token from input */
 Token get_next_token(const char *input, int *pos) {
-    Token token = {TOKEN_ERROR, "", current_line, ERROR_NONE};
-    char c;
+  Token token = {TOKEN_ERROR, "", current_line, ERROR_NONE};
+  char c;
 
-    // Skip whitespace and track line numbers
-    while ((c = input[*pos]) != '\0' && (c == ' ' || c == '\n' || c == '\t')) {
-        if (c == '\n') {
-            current_line++;
-        }
-        (*pos)++;
+  // Skip whitespace and track line numbers
+  while ((c = input[*pos]) != '\0' && (c == ' ' || c == '\n' || c == '\t')) {
+    if (c == '\n') {
+      current_line++;
     }
+    (*pos)++;
+  }
 
-    if (input[*pos] == '\0') {
-        token.type = TOKEN_EOF;
-        strcpy(token.lexeme, "EOF");
-        return token;
+  if (input[*pos] == '\0') {
+    token.type = TOKEN_EOF;
+    strcpy(token.lexeme, "EOF");
+    return token;
+  }
+
+  c = input[*pos];
+
+  // TODO: Add comment handling here
+
+  // Handle numbers
+  if (isdigit(c)) {
+    int i = 0;
+    do {
+      token.lexeme[i++] = c;
+      (*pos)++;
+      c = input[*pos];
+    } while (isdigit(c) && i < sizeof(token.lexeme) - 1);
+
+    token.lexeme[i] = '\0';
+    token.type = TOKEN_NUMBER;
+    return token;
+  }
+
+  // TODO: Add keyword and identifier handling here
+  // Hint: You'll have to add support for keywords and identifiers, and then
+  // string literals
+
+  // TODO: Add string literal handling here
+
+  // Handle operators
+  if (c == '+' || c == '-') {
+    if (last_token_type == 'o') {
+      // Check for consecutive operators
+      token.error = ERROR_CONSECUTIVE_OPERATORS;
+      token.lexeme[0] = c;
+      token.lexeme[1] = '\0';
+      (*pos)++;
+      return token;
     }
-
-    c = input[*pos];
-
-    // TODO: Add comment handling here
-
-    // Handle numbers
-    if (isdigit(c)) {
-        int i = 0;
-        do {
-            token.lexeme[i++] = c;
-            (*pos)++;
-            c = input[*pos];
-        } while (isdigit(c) && i < sizeof(token.lexeme) - 1);
-
-        token.lexeme[i] = '\0';
-        token.type = TOKEN_NUMBER;
-        return token;
-    }
-
-    // TODO: Add keyword and identifier handling here
-    // Hint: You'll have to add support for keywords and identifiers, and then string literals
-
-    // TODO: Add string literal handling here
-
-    // Handle operators
-    if (c == '+' || c == '-') {
-        if (last_token_type == 'o') {
-            // Check for consecutive operators
-            token.error = ERROR_CONSECUTIVE_OPERATORS;
-            token.lexeme[0] = c;
-            token.lexeme[1] = '\0';
-            (*pos)++;
-            return token;
-        }
-        token.type = TOKEN_OPERATOR;
-        token.lexeme[0] = c;
-        token.lexeme[1] = '\0';
-        last_token_type = 'o';
-        (*pos)++;
-        return token;
-    }
-
-    // TODO: Add delimiter handling here
-
-    // Handle invalid characters
-    token.error = ERROR_INVALID_CHAR;
+    token.type = TOKEN_OPERATOR;
     token.lexeme[0] = c;
     token.lexeme[1] = '\0';
+    last_token_type = 'o';
     (*pos)++;
     return token;
+  }
+
+  // TODO: Add delimiter handling here
+
+  // Handle invalid characters
+  token.error = ERROR_INVALID_CHAR;
+  token.lexeme[0] = c;
+  token.lexeme[1] = '\0';
+  (*pos)++;
+  return token;
 }
 
-// This is a basic lexer that handles numbers (e.g., "123", "456"), basic operators (+ and -), consecutive operator errors, whitespace and newlines, with simple line tracking for error reporting.
+// This is a basic lexer that handles numbers (e.g., "123", "456"), basic
+// operators (+ and -), consecutive operator errors, whitespace and newlines,
+// with simple line tracking for error reporting.
 
 int main() {
-    const char *input = "123 + 456 - 789\n1 ++ 2"; // Test with multi-line input
-    int position = 0;
-    Token token;
+  const char *input = "123 + 456 - 789\n1 ++ 2"; // Test with multi-line input
+  int position = 0;
+  Token token;
 
-    printf("Analyzing input:\n%s\n\n", input);
+  printf("Analyzing input:\n%s\n\n", input);
 
-    do {
-        token = get_next_token(input, &position);
-        print_token(token);
-    } while (token.type != TOKEN_EOF);
+  do {
+    token = get_next_token(input, &position);
+    print_token(token);
+  } while (token.type != TOKEN_EOF);
 
-    return 0;
+  return 0;
 }
